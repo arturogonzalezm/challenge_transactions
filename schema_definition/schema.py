5ff313cc4e06b7a91ae846cc90ea7e793488694b
arturogonzalezm <arturo@arturosolutions.com.au>
2021-10-16 02:25:56 +1100

    Implementing reports on transactions_reporting.ipynb

--- schema_definition/schema.py
+++ schema_definition/schema.py
@@ -1,4 +1,4 @@
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType, ShortType, TimestampType
+from pyspark.sql.types import *
 
 bronze_schema = StructType([ \
     StructField("AccountID", IntegerType(), True), \
@@ -17,6 +17,29 @@ bronze_schema = StructType([ \
     StructField("Fibre", StringType(), True), \
     StructField("LastUpdatedDate", StringType(), True), \
     StructField("PropertyType", StringType(), True), \
-    StructField("PostCode", IntegerType(), True)
+    StructField("PostCode", IntegerType(), True) \
  \
     ])
+
+gold_schema = StructType([ \
+    StructField("AccountID", IntegerType(), True), \
+    StructField("CODE", IntegerType(), True), \
+    StructField("ImplementedDate", TimestampType(), True), \
+    StructField("ActiveIndicator", IntegerType(), True), \
+    StructField("AccountType", StringType(), True), \
+    StructField("Service", StringType(), True), \
+    StructField("BU", StringType(), True), \
+    StructField("RequestDate", TimestampType(), True), \
+    StructField("AccountStatus", StringType(), True), \
+    StructField("StatusCode", IntegerType(), True), \
+    StructField("Amount", DoubleType(), True), \
+    StructField("Version", StringType(), True), \
+    StructField("AgentID", IntegerType(), True), \
+    StructField("Fibre", StringType(), True), \
+    StructField("LastUpdatedDate", TimestampType(), True), \
+    StructField("PropertyType", StringType(), True), \
+    StructField("PostCode", IntegerType(), True), \
+    StructField("HashKey", StringType(), True), \
+    StructField("Response", ShortType(), True)
+
+])
